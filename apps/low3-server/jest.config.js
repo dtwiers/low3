72b81dbf3d9ac7d9dee82c55d23fe8e52c5a52fb
Derek Wiers <dtwiers08@gmail.com>
2022-02-22 07:34:05 -0600

    initial commit

---
+++ apps/low3-server/jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  displayName: 'low3-server',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/low3-server',
+};
