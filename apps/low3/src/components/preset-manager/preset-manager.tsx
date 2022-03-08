69d29bd359517114e8cf0028d357ea2854da843b
Derek Wiers <dtwiers08@gmail.com>
2022-03-08 08:45:27 -0600

    initialize preset components

---
+++ apps/low3/src/components/preset-manager/preset-manager.tsx
@@ -0,0 +1,14 @@
+import './preset-manager.module.css';
+
+/* eslint-disable-next-line */
+export interface PresetManagerProps {}
+
+export function PresetManager(props: PresetManagerProps) {
+  return (
+    <div>
+      <h1>Welcome to PresetManager!</h1>
+    </div>
+  );
+}
+
+export default PresetManager;
---
+++ apps/low3/src/components/preset/preset.tsx
@@ -0,0 +1,14 @@
+import './preset.module.css';
+
+/* eslint-disable-next-line */
+export interface PresetProps {}
+
+export function Preset(props: PresetProps) {
+  return (
+    <div>
+      <h1>Welcome to Preset!</h1>
+    </div>
+  );
+}
+
+export default Preset;
